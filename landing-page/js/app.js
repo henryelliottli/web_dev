207f427948801db3bc051b55c34a3ecdb7c70751
henryelliottli <li.henry.elliott@gmail.com>
2021-05-14 00:43:06 +0800

    feat: adding scrolled feature to js file
    app.js - added addActiveClass which adds the sections when website is opened

--- landing-page/js/app.js
+++ landing-page/js/app.js
@@ -17,6 +17,11 @@
  * Define Global Variables
  * 
 */
+const hamburgerButton = document.querySelector("#hamburger_button");
+const nav_menu_overlay = document.querySelector('#nav_menu_overlay');
+const sections = document.querySelectorAll('section');
+const section__links_desktop = document.querySelector(".section__links_desktop");
+const section__links_mobile = document.querySelector(".section__links_mobile");;
 
 
 /**
@@ -25,17 +30,6 @@
  * 
 */
 
-
-
-/**
- * End Helper Functions
- * Begin Main Functions
- * 
-*/
-
-// build the nav
-const hamburgerButton = document.querySelector("#hamburger_button");
-const nav_menu_overlay = document.querySelector('#nav_menu_overlay');
 
 function triggerOverlay(){
 
@@ -57,11 +51,72 @@ function removeOverlayForDesktop(){
     }
 }
 
-function sayHello(){
-    section3.scrollIntoView({
-               behavior: "smooth",
+function createSections() {
+    const fragment1 = document.createDocumentFragment();
+    const fragment2 = document.createDocumentFragment();
+    sections.forEach(element => {
+        let new_ul1 = document.createElement("ul");
+        let new_ul2 = document.createElement("ul");
+        new_ul1.setAttribute("class","nav_items");
+        new_ul1.setAttribute("data-nav",`${element.getAttribute('data-nav')}`);
+        new_ul1.innerText = `${element.getAttribute('data-nav')}`;
+        new_ul2.innerHTML += `<ul class ="nav_items" data-nav ="${element.getAttribute('data-nav')}"> ${element.getAttribute('data-nav')} </ul>`;
+        
+        new_ul1.addEventListener('click',function () {
+            element.scrollIntoView({
+                behavior: 'smooth'
+            })
+        })
+                
+        new_ul2.addEventListener('click',function () {
+            element.scrollIntoView({
+                behavior: 'smooth'
+            })
+        })
+
+        fragment1.appendChild(new_ul1);
+        fragment2.appendChild(new_ul2);
+    })
+    section__links_desktop.appendChild(fragment1);
+    section__links_mobile.appendChild(fragment2);
+}
+
+function appendEmail(){
+    let email = document.createElement('ul');
+    email.innerHTML = `<a class ="nav_items"><a href= "/">Send an Email</a>`;
+    section__links_mobile.appendChild(email);
+}
+
+function inViewport (element) {
+    const viewportSize = element.getBoundingClientRect();
+
+    return (
+        viewportSize.top >= 0 &&
+        viewportSize.x >= 0 &&
+        viewportSize.bottom <= (document.documentElement.clientHeight || window.innerHeight) &&
+        viewportSize.right <= (document.documentElement.clientWidth || window.innerWidth)
+    );
+}
+
+function addActiveClass (){
+    sections.forEach(element => {
+        const nav_item = document.querySelector(".section__links_desktop").querySelector(`[data-nav*="${element.getAttribute('data-nav')}"]`);
+        if (inViewport(element)){
+            element.classList.add("your-active-class");
+            nav_item.classList.add("active-section");
+        }
+        else{
+            element.classList.remove("your-active-class");
+            nav_item.classList.remove("active-section");
+        }
     });
 }
+
+/**
+ * End Helper Functions
+ * Begin Main Functions
+ * 
+*/
 
 
 // build the overlay
@@ -72,7 +127,11 @@ nav_menu_overlay.addEventListener('click',triggerOverlay);
 
 window.addEventListener('resize', removeOverlayForDesktop);
 
+window.addEventListener('scroll',addActiveClass)
+
 // Add class 'active' to section when near top of viewport
+
+
 
 
 // Scroll to anchor ID using scrollTO event
@@ -83,6 +142,9 @@ window.addEventListener('resize', removeOverlayForDesktop);
  * Begin Events
  * 
 */
+createSections();
+appendEmail();
+
 
 // Build menu 
 
