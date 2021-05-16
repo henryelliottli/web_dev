2173a8190e0fc65ba08f6cbc3148742eb7c5f40a
henryelliottli <li.henry.elliott@gmail.com>
2021-05-16 23:21:37 +0800

    feat:initializing server code set up

--- weather-journal-app/server.js
+++ weather-journal-app/server.js
@@ -2,18 +2,28 @@
 projectData = {};
 
 // Require Express to run server and routes
-
+const express = require('express');
 // Start up an instance of app
-
+const app = express();
+/* Dependencies */
+const bodyParser = require('body-parser')
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
 // Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
+//output that server is running
+const port = 8000;
+
+const server = app.listen(port, listening);
+function listening(){
+    console.log(`server is running on port ${port}`);
+}
 
 // Setup Server
